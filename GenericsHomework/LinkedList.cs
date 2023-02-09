f793275e62bd20efb35484a59e7c7d11930e2159
Calvin Crawford <ccrawford8@ewu.edu>
2023-02-09 15:40:26 -0800

    hw5 setup

---
+++ GenericsHomework/LinkedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Node<T>
+{
+    //Node class needs constructor that takes a value
+    // define node class
+    //
+
+
+    public Node append()
+    {
+        return new Node();
+    }
+    public void clear()
+    {
+
+    }
+    public bool exists(Node node)
+    {
+        return exists(node, false);
+    }
+    public override string toString()
+    {
+        return "";
+    }
+
+}
+
