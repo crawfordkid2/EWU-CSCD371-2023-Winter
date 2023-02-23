a51330177a0c112f0f23664c5b5a59f81c1a86b1
George <georgekoralski@hotmail.com>
2023-02-22 16:49:46 -0800

    updated fixes

--- Calculate/Calculate/Program.cs
+++ Calculate/Calculate/Program.cs
@@ -1,29 +1,33 @@
 ﻿namespace Calculate;
-    public class Program
+public class Program
+{
+    public Action<string> WriteLine { get; init; } = x => Console.WriteLine(x);
+    public Func<string?> ReadLine { get; init; } = () => Console.ReadLine();
+
+    public Program() { }
+
+    public static void Main()
     {
-        public Action<string> WriteLine { get; init; } = x => Console.WriteLine(x);
-        public Func<string?> ReadLine { get; init; } = () => Console.ReadLine();
-        public static void Main()
+        Program program = new ();
+        Calculator calc = new ();
+
+        while (true)
         {
-            var calc = new Calculator();
+            program.WriteLine("Type a Mathmatical Equation using whole numbers\n" +
+                "(Ex: x + y, x - y, x * y, x / y)\n" +
+                "make sure to put a space between characters and symbols");
+            var input = program.ReadLine();
+            if (string.IsNullOrEmpty(input))
+                break;
 
-            while(true)
+            if (calc.TryCalculate(input, out var result))
             {
-                Console.WriteLine("Type a Mathmatical Equation " +
-                    "\n (Ex: x+y, x-y, x*y, x/y)" +
-                    "\n make sure to put a space between characters and symbols");
-                var input = Console.ReadLine();
-                if (string.IsNullOrEmpty(input)) break;
-                if(calc.TryCalculate(input,out var result))
-                {
-                    Console.WriteLine(result); break;
-
-                }
-                else
-                {
-                    Console.WriteLine("Invalid equation ");
-                }
+                program.WriteLine($"Result is: {result}");
+            }
+            else
+            {
+                program.WriteLine("Invalid input");
             }
         }
-
     }
+}
